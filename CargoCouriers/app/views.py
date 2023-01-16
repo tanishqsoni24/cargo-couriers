abba1044d705ecc66c0f2da33bd307c731917f79
Tanishq Soni <tanishqsoni0309@gmail.com>
2023-01-17 01:37:18 +0530

    commit-5

--- CargoCouriers/app/views.py
+++ CargoCouriers/app/views.py
@@ -10,11 +10,11 @@ def login(request):
     data_dict = dict()
     data_dict['Navbar'] = 'base/navbar.html'
     data_dict['Data'] = 'forms/login.html'
-    return render(request, 'base/base.html', data_dict)
+    return render(request, 'forms/login.html', data_dict)
 
 
 def signup(request):
     data_dict = dict()
     data_dict['Navbar'] = 'base/navbar.html'
     data_dict['Data'] = 'forms/signup.html'
-    return render(request, 'base/base.html', data_dict)
+    return render(request, 'forms/signup.html', data_dict)
